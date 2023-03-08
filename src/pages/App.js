e26b818b91f837daf955b7c36d62d96d32dfb944
Matheus Lucena <matheus.supervisor@gmail.com>
2023-03-08 19:09:05 -0300

    Finalizando o codigo, 16.43 min

---
+++ src/pages/App.js
@@ -0,0 +1,15 @@
+import gitLogo from '../assets/github.png';
+import Input from '../components/Input';
+
+import {Container} from './styles';
+
+function App() {
+  return (
+    <Container>
+      <img src={gitLogo} width={72} height={72} />
+      <Input />
+    </Container>
+  );
+}
+
+export default App;
